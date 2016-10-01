d3cca000fabd8f3d70e72ba744e7148324cd5071
Akshay Kadam <akshaykadam0904@gmail.com>
2016-10-01 16:44:25 +0530

    Adding script on Sat Oct  1 16:44:25 IST 2016 :blush:

--- simple-interest.cpp
+++ simple-interest.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	int principle,numberofmonths;
-	double yearlyrateofinterest;
+	int numberofmonths;
+	double principle,yearlyrateofinterest;
 	cin>>principle>>yearlyrateofinterest>>numberofmonths;
-	cout<<setprecision(2)<<fixed<<(principle * (yearlyrateofinterest/ 12) * numberofmonths)*0.01<<endl;
+	cout<<setprecision(2)<<fixed<<(principle*(yearlyrateofinterest/ 12.0)*numberofmonths)*0.01<<endl;
 	return 0;
 }
