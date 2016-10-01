1b722834292293851a8d979d67c345648d7cd921
Akshay Kadam <akshaykadam0904@gmail.com>
2016-10-01 16:29:25 +0530

    Adding script on Sat Oct  1 16:29:25 IST 2016 :blush:

---
+++ simple-interest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int principle,number-of-months;
+	double yearly-rate-of-interest;
+	cin>>principal>>yearly-rate-of-interest>>number-of-months;
+	cout<<priniciple * (yearly-rate-of-interest / 12) * number-of-months<<endl;
+	return 0;
+}
